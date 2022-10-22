fb1ea44a15560ed856e6a23b4ec4ba0ec97dff92
Christian Talavera <ctalaveraw@gmail.com>
2022-10-22 19:27:05 -0400

    fixed "Oh My ZSH" install script

--- timewaster/standup/scripts/install_zsh.sh
+++ timewaster/standup/scripts/install_zsh.sh
@@ -4,4 +4,4 @@ echo -e "\n Installing ZSH... \n"
 sudo pacman -Sy --noconfirm zsh
 
 echo -e "\n Installing Oh My ZSH... \n"
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+wget -O - https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
