a6a8901d67a88c28ebd2cb2570e89e664e060cdb
Christian Talavera <ctalaveraw@gmail.com>
2022-10-22 19:59:49 -0400

    added integration to "Discover" GUI package manager

--- timewaster/standup/scripts/install_plasma.sh
+++ timewaster/standup/scripts/install_plasma.sh
@@ -4,7 +4,7 @@ echo -e "\n Installing updates... \n"
 sudo pacman -Syu --noconfirm
 
 echo -e "\n Installing KDE Plasma desktop environment... \n"
-sudo pacman -Sy --noconfirm xorg plasma plasma-wayland-session kde-applications sddm 
+sudo pacman -Sy --noconfirm xorg plasma plasma-wayland-session kde-applications sddm packagekit-qt5
 
 echo -e "\n Installing SSDM... \n"
 sudo pacman -Sy --noconfirm sddm 
