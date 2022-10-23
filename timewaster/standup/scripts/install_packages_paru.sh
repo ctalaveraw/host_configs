748aafe785e11e735491219870fe1fa4cb38a8af
Christian Talavera <ctalaveraw@gmail.com>
2022-10-23 13:03:34 -0400

    add "brave browser" to script list

--- timewaster/standup/scripts/install_packages_paru.sh
+++ timewaster/standup/scripts/install_packages_paru.sh
@@ -6,9 +6,6 @@ paru -Syu --noconfirm
 echo -e "\n Installing system utilities... \n"
 paru -Sy --noconfirm ntfs3-dkms zfs-linux-zen-git adduser
 
-echo -e "\n Installing system apps... \n"
-paru -Syu --noconfirm 
-
 echo -e "\n Installing development apps... \n"
 paru -Sy --noconfirm powershell visual-studio-code-insiders-bin \
 sublime-text-dev ttf-menlo-powerline-git drawio-desktop
@@ -18,3 +15,6 @@ paru -Sy --noconfirm spotify spotify-adblock-git
 
 echo -e "\n Installing gaming hardware configuration apps... \n"
 paru -Sy --noconfirm openrgb piper
+
+echo -e "\n Installing custom apps... \n"
+paru -Sy --noconfirm brave
