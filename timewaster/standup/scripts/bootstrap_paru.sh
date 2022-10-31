601583287da54a3c4144f431d22cca24c807e32a
Christian Talavera <ctalaveraw@gmail.com>
2022-10-31 18:33:51 -0400

    fixed openRGB package

--- timewaster/standup/scripts/bootstrap_paru.sh
+++ timewaster/standup/scripts/bootstrap_paru.sh
@@ -14,7 +14,7 @@ echo -e "\n Installing multimedia apps... \n"
 paru -Sy --noconfirm spotify spotify-adblock-git 
 
 echo -e "\n Installing gaming hardware configuration apps... \n"
-paru -Sy --noconfirm openrgb piper
+paru -Sy --noconfirm openrgb-bin piper
 
 echo -e "\n Installing custom apps... \n"
 paru -Sy --noconfirm brave
