a80a7a8797fb90e40578ec9485f0ae27f20bce38
Christian Talavera <ctalaveraw@gmail.com>
2022-11-01 00:54:05 -0400

    edited script to accept input for name of user

--- timewaster/standup/scripts/config_user_login.sh
+++ timewaster/standup/scripts/config_user_login.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+echo -e '\n Please enter the name of the new user... \n'
+read NEW_USERNAME
+
 echo -e "\n Granting 'wheel' group admin privileges... \n"
 sudo sed -i '' 's/#[[:blank:]]*\(%wheel[[:blank:]]*ALL=(ALL)[[:blank:]]*ALL.*\)/\1/g' /usr/local/etc/sudoers
 
 echo -e "\n Making user account... \n"
-sudo useradd --comment "admin." --user-group --shell /bin/bash --create-home --home-dir /home/admin admin
+sudo useradd --comment "$NEW_USERNAME" --user-group --shell /bin/bash --create-home --home-dir /home/$NEW_USERNAME $NEW_USERNAME
 
 echo -e "\n Giving sudo access... \n"
-sudo usermod -aG wheel admin
+sudo usermod -aG wheel $NEW_USERNAME
 
 echo -e "\n Setting password... \n"
-sudo passwd admin
+sudo passwd $NEW_USERNAME
