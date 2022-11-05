437994bd7d5fbcbab63bb26c31b192dee889bc3d
Christian Talavera <ctalaveraw@gmail.com>
2022-11-05 12:59:14 -0400

    replacing VSCode Insiders with "VSCodium"

--- timewaster/standup/scripts/bootstrap_paru.sh
+++ timewaster/standup/scripts/bootstrap_paru.sh
@@ -7,8 +7,13 @@ echo -e "\n Installing system utilities... \n"
 paru -Sy --noconfirm ntfs3-dkms zfs-linux-zen-git adduser
 
 echo -e "\n Installing development apps... \n"
-paru -Sy --noconfirm powershell-bin visual-studio-code-insiders-bin \
-sublime-text-4 sublime-merge ttf-menlo-powerline-git drawio-desktop slack-desktop
+paru -Sy --noconfirm powershell-bin sublime-text-4 sublime-merge \
+ttf-menlo-powerline-git drawio-desktop slack-desktop
+
+echo -e "\n Installing VSCodium... \n"
+paru -Sy --noconfirm vscodium-bin \
+vscodium-bin-marketplace \
+vscodium-bin-features 
 
 echo -e "\n Installing multimedia apps... \n"
 paru -Sy --noconfirm spotify spotify-adblock-git 
