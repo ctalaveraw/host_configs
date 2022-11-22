24ce6c46e316a4d6ba581e7ada63eb1aa317f87a
Christian Talavera <ctalaveraw@gmail.com>
2022-11-22 17:06:51 -0500

    fixed reflector script

--- timewaster/standup/scripts/config_pacman.sh
+++ timewaster/standup/scripts/config_pacman.sh
@@ -11,7 +11,7 @@ sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
 echo -e "\n Setting pacman mirrorlist... \n"
 sudo pacman -Sy --noconfirm  reflector pacman-contrib
-sudo reflector --latest 5 --country "United States",Canada --sort rate --save /etc/pacman.d/ mirrorlist --protocol https --download-timeout 15
+sudo reflector --latest 5 --country "United States",Canada --sort rate --save /etc/pacman.d/mirrorlist --protocol https --download-timeout 15
 
 echo -e "\n Upgrading system... \n"
 sudo pacman --noconfirm -Syyu
